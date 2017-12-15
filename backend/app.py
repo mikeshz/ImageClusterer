b33c9f969e327ad4ad94662f5661df15aa302d4e
Michael Song <michael.song@magnetforensics.com>
2017-12-15 10:45:22 -0500

    Got reclustering call working

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,16 @@
-from flask import Flask, jsonify
-from flask_cors import CORS
+from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
 import base64
 
-
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources=r'/api/*')
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 # TODO: Put image encoding and creating the image array below into a real method
 with open("./test.jpg", "rb") as image_file:
-        encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+    encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+with open("./heckert_gnu.small.png", "rb") as image_file2:
+    encoded_string2 = base64.b64encode(image_file2.read()).decode('utf-8')
 
 @app.route('/api/getimages')
 def serve_images():
@@ -24,16 +26,30 @@ def serve_images():
         {
             "src": encoded_string,
             "thumbnail": encoded_string
-        },
+        }
         ]}), 200
 
 @app.route('/api/moveview')
 def serve_move_view():
     pass
 
-@app.route('/api/recluster')
+@app.route('/api/recluster', methods=['POST'])
 def serve_recluster():
-    pass
+    print("selected images: " + request.json['selectedImages'])
+    return jsonify({"images": [
+        {
+            "src": encoded_string2,
+            "thumbnail": encoded_string2
+        },
+        {
+            "src": encoded_string2,
+            "thumbnail": encoded_string2
+        },
+        {
+            "src": encoded_string2,
+            "thumbnail": encoded_string2
+        }
+        ]}), 200
 
 
 if __name__ == '__main__':
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,34 +3,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Gallery from 'react-grid-gallery';
 
-class Recluster extends React.Component {
-    handleClick() {
-        console.log("plz");
-        console.log("ayy " + this.props.selectedImages);
-    }
-
-    componentDidMount() {
-        fetch(`http://www.reddit.com/r/${this.props.selectedImages}.json`)
-            .then(res => {
-                const posts = res.data.data.children.map(obj => obj.data);
-                this.setState({ posts });
-            });
-    }
-
-    render() {
-        return (
-            <div>
-                <h1>{`/r/${this.props.subreddit}`}</h1>
-                <ul>
-                    {this.state.posts.map(post =>
-                        <li key={post.id}>{post.title}</li>
-                    )}
-                </ul>
-            </div>
-        );
-    }
+function Recluster(props) {
+    return (
+        <button onClick={props.onClick}>
+            {props.value}
+            Recluster Images
+        </button>
+    );
 }
-
 
 class ImageClusterer extends React.Component {
     constructor(props){
@@ -59,6 +39,42 @@ class ImageClusterer extends React.Component {
                 }
                 this.setState(imagesArray);
             })
+    }
+
+    renderReclusterBtn(selectedImages) {
+        return (
+            <Recluster
+                onClick={() => this.handleClick(this.getSelectedImages().toString())}
+            />
+        );
+    }
+
+    handleClick(selectedImages) {
+        console.log("selected: " + this.getSelectedImages().toString());
+
+        fetch('//localhost:3001/api/recluster', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                selectedImages: this.getSelectedImages().toString()
+            })
+        }).then(results => {
+            return results.json();
+        }).then(data => {
+            var imagesArray = {"images": [data["images"].length]};
+            for (var i = 0; i < data["images"].length; i++) {
+                imagesArray["images"][i] = {
+                    "src": "data:image/png;base64," + data["images"][i]["src"],
+                    "thumbnail": "data:image/png;base64," + data["images"][i]["thumbnail"],
+                    "thumbnailWidth": 300,
+                    "thumbnailHeight": 170
+                }
+            }
+            this.setState(imagesArray);
+        })
     }
 
     allImagesSelected (images){
@@ -110,12 +126,12 @@ class ImageClusterer extends React.Component {
                     color: "#666"
                 }}>Selected images: {this.getSelectedImages().toString()}</div>
             <div>
-                <button onClick={Recluster}>Recluster Images</button>
+                {this.renderReclusterBtn(this.getSelectedImages().toString())}
             </div>
             <div style={{
                 display: "block",
                 minHeight: "1px",
-                width: "1000px",
+                width: "1500px",
                 border: "1px solid #ddd",
                 overflow: "none"}}>
                 <Gallery
